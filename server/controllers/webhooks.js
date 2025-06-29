1fcdce496a6ddd46cc008032ebcf8c2b3dc6f1e1
Hrishant22 <maheshwarihrishant@gmail.com>
2025-06-29 15:39:10 +0530

    Second Commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,56 +2,53 @@ import { Webhook } from "svix";
 import User from "../models/User.js";
 
 //API Controller function to manage clerk user with database
-export const clerkWebhooks = async(requestAnimationFrame, res) => {
-    try{
-        //Create a svix instance with clerk webhook secret.
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+export const clerkWebhooks = async (req, res) => {
+  try {
+    console.log("✅ Webhook HIT");
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body.toString());
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        //Verifying Headers
-        await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signatue": req.headers["svix-signatue"],
-        })
+    const evt = wh.verify(req.body, {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"]
+    });
 
-        //Getting data from request body
-        const {data, type} =  req.body
+    const { data, type } = evt;
+    console.log('Webhook event type:', type);
 
-        //Switch case for different events
-        switch(type){
-            case 'user.created':{
-                const userData = {
-                    _id:data.id,
-                    email: data.email_addresses[0].email_address,
-                    name: data.first_name + " "+ data.last_name,
-                    image: data.image_url,
-                    resume: ''
-                }
-                await User.create(userData)
-                res.json({})
-                break;
-            }
-            case 'user.updated':{
-                const userData = {
-                    email: data.email_addresses[0].email_address,
-                    name: data.first_name + " "+ data.last_name,
-                    image: data.image_url,
-                }
-                await User.findByIdAndUpdate(data.id, userData)
-                res.json({})
-                break;
-            }
-            case 'user.deleted':{
-                await User.findByIdAndDelete(data.id)
-                res.json({})
-                break;
-            }
+    switch (type) {
+      case 'user.created':
+        await User.create({
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+          resume: ''
+        });
+        break;
 
-            default:
-                break;
-        }
-    }catch (error){
-        console.log(error.message);
-        res.json({success: false, message:'Webhooks erroe'})
+      case 'user.updated':
+        await User.findByIdAndUpdate(data.id, {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+        });
+        break;
+
+      case 'user.deleted':
+        await User.findByIdAndDelete(data.id);
+        break;
+
+      default:
+        console.log("Unhandled webhook type:", type);
     }
-}
+
+    res.status(200).json({ success: true });
+
+  } catch (error) {
+    console.error("Webhook error:", error.message);
+    res.status(400).json({ success: false, message: 'Webhook error' });
+  }
+};
